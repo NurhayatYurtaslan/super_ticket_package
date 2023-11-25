05b34ac79135880abe8753d6d08286e9c04f281b
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-25 19:49:53 +0300

    update 🆕: homepage

--- super_ticket_package/example/lib/home_view.dart
+++ super_ticket_package/example/lib/home_view.dart
@@ -18,55 +18,28 @@ class HomeView extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: SuperTicket(
-        itemCount: 3, // 3 resim
-        arcColor: const Color.fromARGB(255, 133, 74, 217),
+      body: const SuperTicket(
+        itemCount: 4, // 3 resim
+        arcColor: Color.fromARGB(255, 133, 74, 217),
         ticketText: 'Ticket Text',
         colors: [
-          const Color.fromARGB(255, 227, 127, 221),
-          const Color.fromARGB(255, 242, 226, 5),
-          const Color.fromARGB(255, 242, 213, 229),
+          Color.fromARGB(255, 227, 127, 221),
+          Color.fromARGB(255, 242, 226, 5),
+          Color.fromARGB(255, 242, 213, 229),
         ],
-        ticketTextStyle: const TextStyle(
+        ticketTextStyle: TextStyle(
           fontSize: 18,
           fontWeight: FontWeight.bold,
           color: Color.fromARGB(255, 70, 84, 166),
         ),
         ticketTitleText: 'Ticket Title',
-        mainWidget: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width * 0.3,
-          child: Column(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Material(
-                  elevation: 2,
-                  child: Container(
-                    color: Colors.grey,
-                    child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        final imageUrls = [
-                          'https://picsum.photos/200/300?random=1',
-                          'https://picsum.photos/200/300?random=2',
-                          'https://picsum.photos/200/300?random=3',
-                        ];
-                        return Image.network(
-                          imageUrls[index],
-                          fit: BoxFit.cover,
-                        );
-                      },
-                      itemCount: 3, // Resim sayısı
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 2,
-              ),
-            ],
-          ),
-        ),
+        imageList: [
+          'https://picsum.photos/200/300?random=1,'
+              'https://picsum.photos/200/300?random=2,'
+              'https://picsum.photos/200/300?random=3,'
+              'https://picsum.photos/200/300?random=4,'
+        ],
+       
       ),
     );
   }
