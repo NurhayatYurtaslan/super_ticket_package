ea0d1c1099fbae362143ce088d50f425cd9c2405
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-25 19:49:43 +0300

    update 🆕: package

--- super_ticket_package/lib/super_ticket_package_view.dart
+++ super_ticket_package/lib/super_ticket_package_view.dart
@@ -10,7 +10,7 @@ class SuperTicket extends StatelessWidget {
   final String ticketText;
   final String ticketTitleText;
   final List<Color> colors;
-  final Widget mainWidget;
+  final List<String> imageList; // Yeni parametre: resim URL listesi
   final TextStyle ticketTextStyle;
   final TextStyle ticketTitleTextStyle;
 
@@ -21,6 +21,7 @@ class SuperTicket extends StatelessWidget {
     required this.ticketText,
     required this.ticketTitleText,
     required this.colors,
+    required this.imageList, // Yeni parametre: resim URL listesi
     this.ticketTextStyle = const TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.bold,
@@ -30,7 +31,7 @@ class SuperTicket extends StatelessWidget {
       fontSize: 18,
       fontWeight: FontWeight.bold,
       color: Colors.black,
-    ), required this.mainWidget,
+    ),
   }) : super(key: key);
 
   @override
@@ -39,6 +40,8 @@ class SuperTicket extends StatelessWidget {
       padding: const EdgeInsets.all(15),
       itemBuilder: (context, index) {
         Color color = colors[index % colors.length];
+        String imageUrl =
+            imageList[index % imageList.length]; // Resim URL'sini al
         return SizedBox(
           height: MediaQuery.of(context).size.height * 0.25,
           width: double.infinity,
@@ -48,7 +51,7 @@ class SuperTicket extends StatelessWidget {
                 children: [
                   Expanded(
                     child: Container(
-                      color: color, // Doğru renk kullanımı
+                      color: color,
                       child: Center(
                         child: RotatedBox(
                           quarterTurns: 3,
@@ -64,17 +67,80 @@ class SuperTicket extends StatelessWidget {
                       color: Colors.white,
                       padding: const EdgeInsets.all(10),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '$ticketTitleText #${index + 1}',
-                            style: ticketTitleTextStyle,
-                          ),
-                          Expanded(
-                            child: mainWidget,
-                          ),
-                        ],
-                      ),
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '$ticketTitleText #${index + 1}',
+                              style: ticketTitleTextStyle,
+                            ),
+                            SizedBox(
+                              height:
+                                  MediaQuery.of(context).size.height * 0.025,
+                            ),
+                            Expanded(
+                              child: Row(children: [
+                                Expanded(
+                                  child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Row(
+                                          children: [
+                                            Icon(Icons.calendar_today),
+                                            SizedBox(
+                                              width: 5,
+                                            ),
+                                            Expanded(
+                                              child: Text(
+                                                '00 month 2023 00:00',
+                                                style: TextStyle(
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        const Row(
+                                          children: [
+                                            Icon(Icons.location_on_outlined),
+                                            SizedBox(
+                                              width: 5,
+                                            ),
+                                            Expanded(
+                                              child: Text(
+                                                'Destination venue',
+                                                style: TextStyle(
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        ElevatedButton.icon(
+                                            onPressed: () {},
+                                            style: ElevatedButton.styleFrom(
+                                                backgroundColor: Colors.white,
+                                                shape: const StadiumBorder(
+                                                  side: BorderSide(
+                                                      width: 1,
+                                                      color: Colors.blue),
+                                                )),
+                                            icon:
+                                                const Icon(Icons.check_circle),
+                                            label: Text(
+                                              'Buy Tickets'.toUpperCase(),
+                                              style: const TextStyle(
+                                                fontSize: 12,
+                                              ),
+                                            ))
+                                      ]),
+                                ),
+                              ]),
+                            ),
+                          ]),
                     ),
                   ),
                 ],
